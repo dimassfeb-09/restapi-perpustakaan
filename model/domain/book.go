1707f3fb7c2ead7be197739d0e84f21ae972c688
DIMAS FEBRIYANTO <dimasfebriyanto@MacBook-Pro-DIMAS.local>
2022-09-10 22:41:11 +0700

    Add Model Categories and Book

--- model/domain/book.go
+++ model/domain/book.go
@@ -1,1 +1,12 @@
 package domain
+
+import "time"
+
+type Book struct {
+	Id             int       `json:"id"`
+	Name           int       `json:"name"`
+	CategoryId     int       `json:"category_id"`
+	CreateAt       time.Time `json:"create_at"`
+	Stock          int       `json:"stock"`
+	ProductsStatus string    `json:"products_status"`
+}
